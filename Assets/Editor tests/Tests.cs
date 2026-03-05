c2297f17c03da6ba0f9d62e8e3895a9ef3fc7f04
UMRGRS <qxrvynilp@gmail.com>
2026-03-04 18:14:00 -0600

    modified unity-ci.yml

--- Assets/Editor tests/Tests.cs
+++ Assets/Editor tests/Tests.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
 using NueGames.NueDeck.Scripts.Energy;
 using NueGames.NueDeck.Scripts.Enums;
 
